750d59213bec2b1ba72641a2074faefe8f7354d1
greatyun <yjs8237@gmail.com>
2020-05-28 19:57:41 +0900

    커밋

--- src/main/java/com/greatyun/springbootdocker/HomeController.java
+++ src/main/java/com/greatyun/springbootdocker/HomeController.java
@@ -1,12 +1,15 @@
 package com.greatyun.springbootdocker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
     @GetMapping("/")
     public String home() {
+        log.info("test");
         return "hello world V2";
     }
 }
